1b602c48623d31cdd8aafdad15d44025045bffd4
jefersongjr <jeferson_gjr@hotmail.com>
2022-03-17 11:32:12 -0300

    testanto o triangulo, desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,9 +4,21 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(line1, line2, line3) {
+  let trianglePossible = false;
+  if (line1 + line2 < line3 || Math.abs(line1 - line2) > line3) {
+    trianglePossible = false;
+  } else if (line1 + line3 < line2 || Math.abs(line1 - line3) > line2) {
+    trianglePossible = false;
+  } else if (line3 + line2 < line1 || Math.abs(line3 - line2) > line1) {
+    trianglePossible = false;
+  } else {
+    trianglePossible = true;
+  }
+  return trianglePossible;
 }
+
+triangleCheck(10, 14, 8);
 
 // Desafio 13
 function hydrate() {
